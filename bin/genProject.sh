db7c51ddec800ae8a978ce15171668e174bfd859
yiqun <1024@w.cn>
2012-02-28 07:21:00 +0800

    Modifiy the script of generate project

--- bin/genProject.sh
+++ bin/genProject.sh
@@ -8,7 +8,7 @@
  * @since 14:22 26/2/2012
  */
 isset($argv[1]) || die('No project dir' . PHP_EOL);
-!is_dir($argv[1]) || die('Project dir exists' . PHP_EOL);
+!is_dir($argv[1]) || !file_exists($argv[1]. '/index.php') || die('Project dir exists' . PHP_EOL);
 
 define('SYS_ROOT', realpath('../'). '/');
 define('DATE', date('H:i d/m/Y'));
